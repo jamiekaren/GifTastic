48bb7274af59ad58ee40210b0c61afae84eca6d2
Jamie Ferrugiaro <missjamiekaren@gmail.com>
2019-07-07 10:38:22 -0400

    I have a lot of bugs yet to work through, one variable is coming up as not defined but not sure why because I did define it. Decided to just create a new branch to basically start from scratch.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,52 @@
 console.log("Working...");
 
+/*
+Ok, so we need to create buttons and add them to the page with the                                                                                                                                                                                 
+Then, we need to create function to create gifs and add them to html page
+
+*/
+
 //An array to hold the gifs
 let gifs = ["Cat", "Parot"];
+
+// we have to build the url based off of user input (array?) and link...
+
+
+
+
+// Now a function to handle when button is clicked
+$("#add-gif").on("click", function (e) {
+    //event.preventDefault() , prevents form trying to submit itself.
+    //Using a form so user can hit enter insted of clicking
+    e.preventDefault();
+
+    // Grab the text from the input box and store in a variable
+    let gif = $("#gif-input").val();
+
+    // Push that new gif to the array
+    gifs.push(gif);
+
+    console.log(gif);
+
+
+    // Url def works with a search value in there and the rest of the URl, checked in browser
+    let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=xO21dEI2EXTWHMEZju3HSOteEK4SeMKo";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        $(response).prepend("#gifs-view");
+    });
+
+
+    // Call our button function which should handle creating the buttons
+    //for our array
+    renderButtons();
+
+
+});
 
 
 //Function to display the gif buttons up top
@@ -16,34 +61,14 @@ function renderButtons() {
     for (let i = 0; i < gifs.length; i++) {
 
         // Then we generate buttons for each gif in the array.
-        let b = $("<button>");
+        let newButton = $("<button>");
         //Then we add a class
-        b.addClass("gif");
+        newButton.addClass("gif");
         // Then we add a data-attribute with value set to movie at index i
-        b.attr("data-name", gif[i]);
+        newButton.attr("data-name", gif[i]);
         //Providing the button's text with a value of movie at index i
-        b.text(gif[i]);
+        newButton.text(gif[i]);
         //Adding the button to the HTML
-        $("#buttons-view").append(b);
+        $("#buttons-view").append(newButton);
     }
 };
-
-
-// Now a function to handle when button is clicked
-$("#add-gif").on("click", function (e) {
-    //event.preventDefault() , prevents form trying to submit itself.
-    //Using a form so user can hit enter insted of clicking
-    e.preventDefault();
-
-    // Grab the text from the input box and store in a variable
-    let gif = $("#gif-input").val();
-    // Push that new movie to the array
-    gif.push(gifs);
-
-    // Call our button function which should handle creating the buttons
-    //for our array
-    renderButtons();
-});
-
-//Calling buttons at least once to display the initial list of gifs
-renderButtons();
